d31cd27305286678627b5a6ce57fc9d6e610a258
ricardgarcia16 <102313069+ricardgarcia16@users.noreply.github.com>
2023-02-24 16:14:24 +0100

    musica

--- app/src/main/java/com/example/m821/MainActivity.java
+++ app/src/main/java/com/example/m821/MainActivity.java
@@ -3,10 +3,15 @@ package com.example.m821;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mp;
+    Button botonmapa, botonlista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +21,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void btnMapa(View view) {
+        mp = MediaPlayer.create(this, R.raw.quevedo);
+        mp.start();
         Intent mapa = new Intent(this, MapsActivity.class);
+
         startActivity(mapa);
 
     }
 
     public void btnLlista(View view) {
+        mp = MediaPlayer.create(this, R.raw.quevedo);
+        mp.start();
         Intent llista = new Intent(this, Llista.class);
         startActivity(llista);
     }
+
+
+
 }
