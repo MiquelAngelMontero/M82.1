fca67a23c60ff78f29b2231944b4be60a15bb773
MiquelAngelMontero <mmontero2@institutjaumehuguet.cat>
2023-02-24 16:07:26 +0100

    menuPrincipalsensesonido

---
+++ app/src/main/java/com/example/m821/Llista.java
@@ -0,0 +1,14 @@
+package com.example.m821;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Llista extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_llista);
+    }
+}
