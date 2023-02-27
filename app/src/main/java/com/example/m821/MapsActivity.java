4c3358c1c101ac009c07abb54aa1c66a32e9f611
MiquelAngelMontero <mmontero2@institutjaumehuguet.cat>
2023-02-27 22:34:40 +0100

    mapa fet

--- app/src/main/java/com/example/m821/MapsActivity.java
+++ app/src/main/java/com/example/m821/MapsActivity.java
@@ -8,44 +8,40 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.example.m821.databinding.ActivityMapsBinding;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private ActivityMapsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityMapsBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        setContentView(R.layout.activity_maps);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng GCArena = new LatLng(28.102907, -15.456285);   
+        mMap.addMarker(new MarkerOptions().position(GCArena).title("Gran Canaria Arena").icon(BitmapDescriptorFactory.fromResource(R.drawable.quevedomarker))); //Personalitzacio del marcador
+        LatLng WiZink = new LatLng(40.423748, -3.671747);   
+        mMap.addMarker(new MarkerOptions().position(WiZink).title("WiZink Center").icon(BitmapDescriptorFactory.fromResource(R.drawable.quevedomarker))); //Personalitzacio del marcador
+        LatLng StJordi = new LatLng(41.363887, 2.152614);   
+        mMap.addMarker(new MarkerOptions().position(StJordi).title("Sant Jordi Club").icon(BitmapDescriptorFactory.fromResource(R.drawable.quevedomarker))); //Personalitzacio del marcador
+        LatLng Cabaret = new LatLng(37.388188, -5.976860);   
+        mMap.addMarker(new MarkerOptions().position(Cabaret).title("Cabaret Festival Latino").icon(BitmapDescriptorFactory.fromResource(R.drawable.quevedomarker))); //Personalitzacio del marcador
+        LatLng NavArena = new LatLng(42.795883, -1.635303);   
+        mMap.addMarker(new MarkerOptions().position(NavArena).title("Navarra Arena").icon(BitmapDescriptorFactory.fromResource(R.drawable.quevedomarker))); //Personalitzacio del marcador
+        LatLng BEC = new LatLng(43.288535, -2.992691);   
+        mMap.addMarker(new MarkerOptions().position(BEC).title("BEC!").icon(BitmapDescriptorFactory.fromResource(R.drawable.quevedomarker))); //Personalitzacio del marcador
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(GCArena));
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL); //Tipus de mapa
     }
 }
